f21504eca269376436bac11947a43eee27a43ec9
prabhu2904 <uyyalaprabhukumar77@gmail.com>
2025-01-31 14:23:06 -0800

    just printing

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=ROBERT
+PERSON2=RAHEEM
+
+echo " $PERSON1 = hello $PERSON2 ,good morning "
+echo " $PERSON2 = Good morning $PERSON1, how are you "
+echo " $PERSON1 = I AM GOOD $PERSON2, THANKS FOR ASKING" 
