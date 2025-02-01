d1d9f35ba0a77c283c38497ab9615c4b4c49b580
prabhu2904 <uyyalaprabhukumar77@gmail.com>
2025-01-31 16:14:52 -0800

    just printing

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -5,8 +5,10 @@ NUMBER2=$2
 
 SUM=$((NUMBER1 + NUMBER2))
 
-echo "Total :: $SUM"
+echo "Total:: $SUM"
 
-echo "total args passed :: $@"
+echo "How many args passed:: $#"
 
-echo "script name : $0"
+echo "All args passed:: $@"
+
+echo "Script name:: $0"
